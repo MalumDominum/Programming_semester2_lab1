e360f874bbf95f98f6c0c23a2f98b8fcc3efd5ce
Mihail Alekseev <mihailalekseev02@gmail.com>
2020-02-28 11:06:06 +0200

    Creating project

---
+++ Programming_semester2_lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_semester2_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Programming_semester2_lab1/ReaderWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_semester2_lab1
+{
+    class ReaderWriter
+    {
+    }
+}
